ab6b485b468e7c5de359a664658e6af5151ea0b5
samuel.kodytek@guided.cz <MartinSmrdi987>
2022-01-06 15:01:27 +0100

    Slate basics examples

---
+++ SlateBasicsExample1/Source/SlateExample/SAWidget.cpp
@@ -0,0 +1,39 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SAWidget.h"
+
+#include "SlateOptMacros.h"
+
+BEGIN_SLATE_FUNCTION_BUILD_OPTIMIZATION
+
+void SAWidget::Construct(const FArguments& InArgs)
+{
+	OwningHUD = InArgs._OwningHUD;
+	// InArgs._DummyArgument1;
+	// InArgs._DummyArgument2;
+	// ...
+
+	FSlateFontInfo TextStyle = FCoreStyle::Get().GetFontStyle("EmbossedText");
+	TextStyle.Size = 40.f;
+	
+	ChildSlot
+	[
+		SNew(SOverlay)
+		+ SOverlay::Slot()
+		  .HAlign(HAlign_Center) // Popř. HAlign_Fill, HAlign_Left, HAlign_Right
+		  .VAlign(VAlign_Center)
+		[
+			SAssignNew(TextBlock, STextBlock)
+			.Font(TextStyle)
+			.Text(FText::FromString("Player speed: 0"))
+		]
+	];
+}
+
+void SAWidget::SetPlayerSpeed(float Speed)
+{
+	TextBlock->SetText(FText::Format(FTextFormat::FromString("Player speed: {0}"), Speed));
+}
+
+END_SLATE_FUNCTION_BUILD_OPTIMIZATION
