985f928b53e67b2c8716c9a92fa59fdc6058d469
samuel.kodytek <samuel.kodytek@guided.cz>
2022-02-24 15:33:29 +0100

    AI Path Finding

---
+++ PathFinding/Source/PathFinding/PathFinding.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "PathFinding.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, PathFinding, "PathFinding" );
---
+++ PathFinding/Source/PathFinding/PathFindingGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PathFindingGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PATHFINDING_API APathFindingGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
